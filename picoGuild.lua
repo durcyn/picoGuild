b4f4493818d7b7c54d0934bc3d156c6a46ffc52d
durcyn <durcyn@gmail.com>
2012-09-15 19:49:47 -0500

    fuck it, use a cache table upvalue some shit

--- picoGuild.lua
+++ picoGuild.lua
@@ -1,3 +1,32 @@
+local _G=getfenv(0)
+local format = _G.string.format
+local find = _G.string.find
+local floor = _G.math.floor
+local pairs = _G.pairs
+local wipe = _G.table.wipe
+local insert = _G.table.insert
+local sort = _G.table.sort
+local IsInGuild = _G.IsInGuild
+local GetGuildFactionInfo = _G.GetGuildFactionInfo
+local GetGuildInfo = _G.GetGuildInfo
+local GetGuildLevel = _G.GetGuildLevel
+local GetGuildRosterInfo = _G.GetGuildRosterInfo
+local GetGuildRosterMOTD = _G.GetGuildRosterMOTD
+local GetNumGuildMembers = _G.GetNumGuildMembers
+local GetRealZoneText = _G.GetRealZoneText
+local GetText = _G.GetText
+local GuildRoster = _G.GuildRoster
+local IsLoggedIn = _G.IsLoggedIn
+local QueryGuildXP = _G.QueryGuildXP
+local RAID_CLASS_COLORS = _G.RAID_CLASS_COLORS
+local REMOTE_CHAT = _G.REMOTE_CHAT
+local ToggleGuildFrame = _G.ToggleGuildFrame
+local UnitGetGuildXP = _G.UnitGetGuildXP
+local UnitInParty = _G.UnitInParty
+local UnitInRaid = _G.UnitInRaid
+local UnitLevel = _G.UnitLevel
+local UnitName = _G.UnitName
+local UnitSex = _G.UnitSex
 
 ----------------------------
 --      Localization      --
@@ -12,6 +41,8 @@ local L = {
 	["Not in a guild"] = "Not in a guild",
 }
 
+local cache = {}
+local online, total, remote = 0, 0, 0
 
 ------------------------------
 --      Are you local?      --
@@ -19,7 +50,7 @@ local L = {
 
 local mejoin = UnitName("player").." has joined the guild."
 local friends, colors = {}, {}
-for class,color in pairs(RAID_CLASS_COLORS) do colors[class] = string.format("%02x%02x%02x", color.r*255, color.g*255, color.b*255) end
+for class,color in pairs(RAID_CLASS_COLORS) do colors[class] = format("%02x%02x%02x", color.r*255, color.g*255, color.b*255) end
 
 -------------------------------------------
 --      Namespace and all that shit      --
@@ -58,39 +89,22 @@ end
 
 function f:PLAYER_LOGIN()
 --	LibStub("tekKonfig-AboutPanel").new(nil, "picoGuild")
-
 	self:Show()
 	self:RegisterEvent("GUILD_ROSTER_UPDATE")
 	self:RegisterEvent("GUILD_XP_UPDATE")
 	self:RegisterEvent("CHAT_MSG_SYSTEM")
 	self:RegisterEvent("PLAYER_ENTERING_WORLD")
-	self:RegisterEvent("PLAYER_LOGOUT")
-
-	if IsInGuild() then
-		QueryGuildXP()
-		GuildRoster()
-		SortGuildRoster("rank")
-	end
-
 	self:UnregisterEvent("PLAYER_LOGIN")
 	self.PLAYER_LOGIN = nil
+	f:GUILD_XP_UPDATE()
+	f:GUILD_ROSTER_UPDATE()
 end
-
 
 function f:UpdateText()
 	if IsInGuild() then
-	local total, online = GetNumGuildMembers()
 		local currentXP, remainingXP = UnitGetGuildXP("player")
 		local level = GetGuildLevel() + currentXP/(currentXP + remainingXP)
-		local remotes = 0 
-		for i=1,online do
-			local _, _, _, _, _, _, _, _, connected, _, _, _, _, mobile = GetGuildRosterInfo(i)
-			if connected and mobile then	
-				remotes = remotes + 1
-			end
-		end
-		online = online - remotes
-		dataobj.text = string.format("Lv%.1f - %d/%d (%d)", math.floor(level*10)/10, online, total, remotes)
+		dataobj.text = format("Lv%.1f - %d/%d (%d)", floor(level*10)/10, remote and (online - remote) or online, total, remote)
 	else dataobj.text = L["No Guild"] end
 end
 
@@ -98,26 +112,34 @@ end
 --      Event Handlers      --
 ------------------------------
 --
-function f:PLAYER_LOGOUT()
-	if IsInGuild() then
-		SortGuildRoster("rank")
-	end
-end
 
-function f:PLAYER_ENTERING_WORLD()
+function f:GUILD_XP_UPDATE()
 	if IsInGuild() then
 		QueryGuildXP()
 		GuildRoster()
+		f:UpdateText()
+	end
+end
+function f:GUILD_ROSTER_UPDATE()
+	if IsInGuild() then
+		wipe(cache)
+		total, online = GetNumGuildMembers()
+		remote = 0
+		for i = 1, total do
+			local name, rank, rnum, level, class, area, pnote, onote, connected, status, engclass, points, pointrank, mobile = GetGuildRosterInfo(i)
+			if mobile then area = REMOTE_CHAT end
+			insert(cache, {name=name,rank=rank,rnum=rnum,level=level,class=class,area=area,pnote=pnote,onote=onote,connected=connected,status=status,engclass=engclass,points=points,pointrank=pointrank,mobile=mobile})
+			if mobile then remote = remote + 1 end
+		end
+		sort(cache, function(a,b) return a.rnum < b.rnum end)
+		f:UpdateText()
 	end
 end
 
+
 function f:CHAT_MSG_SYSTEM(event, msg)
-	if string.find(msg, L["has come online"]) or string.find(msg, L["has gone offline"]) or msg == mejoin then dirty = true end
+	if find(msg, L["has come online"]) or find(msg, L["has gone offline"]) or msg == mejoin then dirty = true end
 end
-
-
-f.GUILD_ROSTER_UPDATE = f.UpdateText
-f.GUILD_XP_UPDATE = f.UpdateText
 
 
 ------------------------
@@ -146,41 +168,35 @@ function dataobj.OnEnter(self)
 		tip:AddLine(GetGuildRosterMOTD(), 0, 1, 0, true)
 		tip:AddLine(" ")
 
-		tip:AddLine(string.format("Rep:|cffffffff %s %d%% (%d/%d)", factionStandingtext, barValue / barMax * 100, barValue, barMax))
+		tip:AddLine(format("Rep:|cffffffff %s %d%% (%d/%d)", factionStandingtext, barValue / barMax * 100, barValue, barMax))
 		tip:AddLine(" ")
 
 		local mylevel, myarea = UnitLevel("player"), GetRealZoneText()
-		local total, online = GetNumGuildMembers()
-		local remotes = 0
-		for i=1,online do
-			local name, rank, rankIndex, level, class, area, note, officernote, connected, status, engclass, points, pointrank, mobile = GetGuildRosterInfo(i)
-			if connected and mobile then	
-				remotes = remotes + 1
-			else
-				local cc = RAID_CLASS_COLORS[engclass]
+
+		for k,v in pairs(cache) do
+			if v.connected and not v.mobile then
+				local cc = RAID_CLASS_COLORS[v.engclass]
 				local lr, lg, lb, ar, ag, ab = 0, 1, 0, 1, 1, 1
-				if level < (mylevel - 5) then lr, lg, lb = .6, .6, .6
-				elseif level > (mylevel + 5) then lr, lg, lb = 1, 0, 0 end
+				if v.level < (mylevel - 5) then lr, lg, lb = .6, .6, .6
+				elseif v.level > (mylevel + 5) then lr, lg, lb = 1, 0, 0 end
 				local grouped = false
 				if UnitInParty(name) or UnitInRaid(name) then grouped = true end
-				if area == myarea then ar, ag, ab = 0, 1, 0 end
-				local levelcolor = (level >= (mylevel - 5) and level <= (mylevel + 5)) and "|cff00ff00" or ""
-				tip:AddMultiLine(grouped and "+" or " ", (level < 10 and "0" or "")..level, name, area or "???", note, officernote, rank, 0, grouped and 1 or 0, 0, lr,lg,lb, cc.r,cc.g,cc.b, ar,ag,ab, nil,nil,nil, 1,1,0, .7,.7,1)
+				if v.area == myarea then ar, ag, ab = 0, 1, 0 end
+				local levelcolor = (v.level >= (mylevel - 5) and v.level <= (mylevel + 5)) and "|cff00ff00" or ""
+				tip:AddMultiLine(grouped and "+" or " ", (v.level < 10 and "0" or "")..v.level, v.name, v.area or "???", v.pnote, v.onote, v.rank, 0, grouped and 1 or 0, 0, lr,lg,lb, cc.r,cc.g,cc.b, ar,ag,ab, nil,nil,nil, 1,1,0, .7,.7,1)
 			end
 		end
-		if remotes > 0 then 
+		if remote > 0 then 
 			tip:AddLine(" ")
-			for i=1,online do
-				local name, rank, rankIndex, level, class, area, note, officernote, connected, status, engclass, points, pointrank, mobile = GetGuildRosterInfo(i)
-				if connected and mobile then
-					local cc = RAID_CLASS_COLORS[engclass]
+			for k,v in pairs(cache) do
+				if v.connected and v.mobile then
+					local cc = RAID_CLASS_COLORS[v.engclass]
 					local lr, lg, lb, ar, ag, ab = 0, 1, 0, 1, 1, 1
-					if level < (mylevel - 5) then lr, lg, lb = .6, .6, .6
-					elseif level > (mylevel + 5) then lr, lg, lb = 1, 0, 0 end
-					if mobile then area = REMOTE_CHAT end
-					if area == myarea then ar, ag, ab = 0, 1, 0 end
-					local levelcolor = (level >= (mylevel - 5) and level <= (mylevel + 5)) and "|cff00ff00" or ""
-					tip:AddMultiLine(" ", (level < 10 and "0" or "")..level, name, area or "???", note, officernote, rank, 0, 0, 0, lr,lg,lb, cc.r,cc.g,cc.b, ar,ag,ab, nil,nil,nil, 1,1,0, .7,.7,1)
+					if v.level < (mylevel - 5) then lr, lg, lb = .6, .6, .6
+					elseif v.level > (mylevel + 5) then lr, lg, lb = 1, 0, 0 end
+					if v.area == myarea then ar, ag, ab = 0, 1, 0 end
+					local levelcolor = (v.level >= (mylevel - 5) and v.level <= (mylevel + 5)) and "|cff00ff00" or ""
+					tip:AddMultiLine(" ", (v.level < 10 and "0" or "")..v.level, v.name, v.area or "???", v.pnote, v.onote, v.rank, 0, 0, 0, lr,lg,lb, cc.r,cc.g,cc.b, ar,ag,ab, nil,nil,nil, 1,1,0, .7,.7,1)
 				end
 			end
 		end
